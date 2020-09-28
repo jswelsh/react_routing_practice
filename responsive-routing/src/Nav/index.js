886bc33038ab73e5d3af38834c57efb2e2309706
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-27 23:41:05 -0700

    add usercard component and styling for user cards

--- responsive-routing/src/Nav/index.js
+++ responsive-routing/src/Nav/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Nav.css';
 import logo from './logo.svg';
 
+
 const Nav = () => (
   <nav>
-    <img src={logo} alt="Logo" />
+    <img src={logo} className="App-logo" alt="logo" />
   </nav>
 );
 
