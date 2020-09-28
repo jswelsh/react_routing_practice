a1495997a6b8b64ec4a6bc280d53e3184171a8c4
jswelsh <james.sinclair.welsh@gmail.com>
2020-09-28 00:39:46 -0700

    added mock data, and fix formatting

--- responsive-routing/src/App.js
+++ responsive-routing/src/App.js
@@ -1,15 +1,74 @@
 
+
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom'; // add Switch
+import Media from 'react-media'; // add Media
 import Nav from './Nav';
+import UsersList from './Users/UsersList'; // add UsersList
+import UsersDetails from './Users/UsersDetails'; // add UsersDetails
+import UsersDashboard from './Users/UsersDashboard';
+import mockData from './MOCK_DATA.json'
+import './App.css';
+
 
 class App extends Component {
+
+  state = {
+    users: [ ...mockData ]
+  }  // ...
+
+
+
   render() {
     return (
-      <div>
+      <div className="App">
         <Nav />
+        <Media query="(max-width: 599px)">
+          {matches =>
+            matches ? (
+              <Switch>
+                <Route
+                  exact
+                  path="/users"
+                  render={props => (
+                    <UsersList users={this.state.users} {...props} />
+                  )}
+                />
+                <Route
+                  path="/users/:id"
+                  render={props => (
+                    <UsersDetails
+                      user={
+                        this.state.users.filter(
+                          user =>
+                            user.id === parseInt(props.match.params.id, 10)
+                        )[0]
+                      }
+                      {...props}
+                    />
+                  )}
+                />
+                <Redirect from="/" to="/users"/>
+                <Redirect from="/dashboard" to="/users"/>
+              </Switch>
+            ) : (
+              <Switch>
+                <Route
+                  path="/dashboard"
+                  render={props => (
+                    <UsersDashboard users={this.state.users} {...props} />
+                  )}
+                />
+                <Redirect from="/" to="/dashboard"/>
+                <Redirect from="/users" to="/dashboard"/>
+              </Switch>
+            )
+          }
+        </Media>
       </div>
     );
   }
+
 }
 
 export default App;
